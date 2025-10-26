5006fda0b4ccbbbb72e0d5d530d1fbe6ec24b036
sumitkumawat090912-dot <sumitkumawat090912@gmail.com>
2025-10-26 20:00:59 +0530

    main

--- main.py
+++ main.py
@@ -1177,7 +1177,7 @@ async def txt_handler(bot: Client, m: Message):
 
             #elif "d1d34p8vz63oiq" in url or "sec1.pw.live" in url:
             elif "childId" in url and "parentId" in url:
-                url = f"https://anonymouspwplayer-0e5a3f512dec.herokuapp.com/pw?url={url}&token={pwtoken}"
+                url = f"https://anonymouspwplayer-25261acd1521.herokuapp.com/pw?url={url}&token={pw_token}"
                            
             if ".pdf*" in url:
                 url = f"https://dragoapi.vercel.app/pdf/{url}"
